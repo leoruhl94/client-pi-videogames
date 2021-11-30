7f8cd0cbd8bc3a689c2b45115a3f6104bd0e525d
leoruhl94 <leoruhl94@gmail.com>
2021-11-30 20:01:15 -0300

    seteo de intervalo para evitar que se duerma el back

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import axios from "axios";
+
+
+setInterval(() => {
+    axios.get("https://the-games-api.herokuapp.com/interval")
+      .then(data => console.log(data.data))
+},1700000)
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
